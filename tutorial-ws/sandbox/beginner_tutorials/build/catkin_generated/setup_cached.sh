9e7ca990d371f6706eb8a5271e8f35285156ea1b
dtsbourg <dylan.bourgeois@epfl.ch>
2016-02-28 15:57:59 +0100

    test talker and listener

---
+++ tutorial-ws/sandbox/beginner_tutorials/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials/build/devel/lib:/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials/build/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials/build/devel/lib/pkgconfig:/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials/build/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials/build/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/dtsbourg/indigo_workspace/sandbox/beginner_tutorials:/opt/ros/indigo/share:/opt/ros/indigo/stacks"
